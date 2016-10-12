c1f5d922d42b1faac3ca4abc62e610ce11a4171b
Christian Stewart <christian@paral.in>
2016-10-12 13:45:19 -0400

    Break ComponentList into separate file

--- component.go
+++ component.go
@@ -12,51 +12,7 @@ import (
 
 var ComponentNotExistError error = errors.New("Component or state does not exist.")
 
-// Top-level list of components
-var componentListKey []byte = []byte("components")
 var componentDetailsKey []byte = []byte("component")
-
-type ComponentList struct {
-	Data dbproto.ComponentList
-}
-
-func (c *ComponentList) WriteToDb(db *bolt.DB) error {
-	return db.Update(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket(globalBucketName)
-		data, err := proto.Marshal(&c.Data)
-		if err != nil {
-			return err
-		}
-		return bkt.Put(componentListKey, data)
-	})
-}
-
-func (c *ComponentList) ContainsComponent(name string) bool {
-	for _, nm := range c.Data.ComponentName {
-		if nm == name {
-			return true
-		}
-	}
-	return false
-}
-
-func (c *ComponentList) LoadFromDb(db *bolt.DB) error {
-	return db.Update(func(tx *bolt.Tx) error {
-		bkt := tx.Bucket(globalBucketName)
-		data := bkt.Get(componentListKey)
-		c.Data.Reset()
-		if data != nil {
-			if err := proto.Unmarshal(data, &c.Data); err != nil {
-				return err
-			}
-		}
-		dta, err := proto.Marshal(&c.Data)
-		if err != nil {
-			return err
-		}
-		return bkt.Put(componentListKey, dta)
-	})
-}
 
 // A instantiated component
 type Component struct {
